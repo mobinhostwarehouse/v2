187e2f8939ff8a7eb5d5eed155dbdc114b42d39f
thechameleonteam <101596952+thechameleonteam@users.noreply.github.com>
2022-07-24 16:53:39 +0430

    Update index4.js

--- OOP/index4.js
+++ OOP/index4.js
@@ -25,11 +25,11 @@ async function X() {
     };
     let Docs = [
         {
-            username : "arch" ,
+            username : "admin" ,
             password : "1234"
 
         } , {
-            username : "tooor" ,
+            username : "sajad" ,
             password : "12345"
         }
     ];
@@ -44,8 +44,26 @@ async function X() {
     // const MongoFoundObj = await users.findOne( { username : "xyz" } );
     // console.log( MongoFoundObj ); //null
 
-    // const f = await users.find( { username : "sajjad" } ); ????
-    // console.log( f );
+    // let MongoSearchResult = await MongoCollectionObj.find( {} ).toArray();
+    // let MongoSearchResult = await MongoCollectionObj.find( { password : "123" } , { projection: { _id: 0, username: 1 } } ).toArray(); // username field ONLY
+    // let MongoSearchResult = await MongoCollectionObj.find( { password : "123" } , { projection: { password : 0 } ).toArray(); // all fields except password field
+    // let MongoSearchResult = await MongoCollectionObj.find().sort( { username : 1 } ).toArray();  // sort based on username field ( also { username : -1 } )
+    // let MongoSearchResult = await MongoCollectionObj.find().limit(5).toArray();
+
+    // like insert :
+    // MongoCollectionObj.deleteOne();
+    // MongoCollectionObj.deleteMany();
+
+    // let c = await MongoCollectionObj.drop(); [ true / false ]
+
+    // let z = await MongoCollectionObj.updateOne( { username : "admin" } , { $set: { password : "123" } } );
+    // .updateMany
+    // .replaceOne --> a new defined doc is replaced 
+
+    // { status: { $in: ['OK', 'InProgress'] } } // or $nin
+    // { status: { $ne: 'Failed' } }
+    // { status: 'InProgress' , age : { $lt : 22 } } // Logical AND , <
+    // { $or: [ { status: 'Failed' } , { age : { $gt : 22 } } ] } // Logical OR , >
     MDBClose( MDBSession );
 }
 
